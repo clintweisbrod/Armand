e1ebd2a8db53080f2e628e6901896de8b1c9e22a
Clint Weisbrod <clintweisbrod@gmail.com>
2014-12-30 16:45:25 -0500

    Added standard comment header.

--- Armand/Source/OpenGL/VBOData.cpp
+++ Armand/Source/OpenGL/VBOData.cpp
@@ -1,3 +1,26 @@
+// ----------------------------------------------------------------------------
+// Copyright (C) 2014 Clint Weisbrod. All rights reserved.
+//
+// VBOData.cpp
+//
+// Helper-class for constructing tightly-packed VBO array data.
+//
+// NOTE: For large amounts of data, interleaved format seems to be a
+// little quicker.
+//
+// THIS SOFTWARE IS PROVIDED BY CLINT WEISBROD "AS IS" AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+// EVENT SHALL CLINT WEISBROD OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// ----------------------------------------------------------------------------
+
+
 #include "stdafx.h"
 
 #include "VBOData.h"
