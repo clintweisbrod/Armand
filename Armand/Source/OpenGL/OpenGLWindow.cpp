25b341492d1ad7fb14a74238cf2fa192c8d8582b
Clint Weisbrod <clintweisbrod@gmail.com>
2015-01-14 10:36:07 -0500

    Moved temporary text below visible dome edge so that it is always readable.

--- Armand/Source/OpenGL/OpenGLWindow.cpp
+++ Armand/Source/OpenGL/OpenGLWindow.cpp
@@ -888,14 +888,14 @@ void OpenGLWindow::render()
 	wstring text;
 	swprintf(infoBuffer, 256, L"Speed: %s", speedStr.c_str());
 	text = infoBuffer;
-	fontRenderer->renderSpherical(text, 15, Vec2f(degToRad(-20.0f), degToRad(5.0f)), Vec4f(1.0f, 1.0f, 1.0f, 1.0f));
+	fontRenderer->renderSpherical(text, 15, Vec2f(degToRad(-50.0f), degToRad(-5.0f)), Vec4f(1.0f, 1.0f, 1.0f, 1.0f));
 
 	Vec3f cameraPos = (Vec3f)mCamera.getUniveralPosition();
 	float_t cameraDistanceFromOrigin = cameraPos.length();
 	wstring distanceStr = getNiceDistanceString(cameraDistanceFromOrigin, 1);
 	swprintf(infoBuffer, 256, L"Origin: %s", distanceStr.c_str());
 	text = infoBuffer;
-	fontRenderer->renderSpherical(text, 15, Vec2f(degToRad(10.0f), degToRad(5.0f)), Vec4f(1.0f, 1.0f, 1.0f, 1.0f));
+	fontRenderer->renderSpherical(text, 15, Vec2f(degToRad(30.0f), degToRad(-5.0f)), Vec4f(1.0f, 1.0f, 1.0f, 1.0f));
 //*/
 
 ///*
